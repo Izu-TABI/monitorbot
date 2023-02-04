87a32b1f7d310562bf8d61e1c6b03f44c2bde062
Izu-TABI <92037081+Izu-TABI@users.noreply.github.com>
2023-02-05 00:12:55 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+import discord
+import os
+from datetime import datetime, timedelta
+
+client=discord.Client(intents=discord.Intents.all())
+
+
+LOG_CH_ID = int(os.environ['LOG_CHANNEL'])
+GUILD_ID = int(os.environ['GUILD_ID'])
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello')
+  
+@client.event
+async def on_voice_state_update(member, before, after) :
+  now = datetime.utcnow() + timedelta(hours=9)
+  
+  if member.guild.id == GUILD_ID and (before.channel != after.channel):
+    if before.channel is None:
+      msg = f'{now:%m/%d-%H:%M}｜**{member.name}**が<#{after.channel.id}>に参加しました。'
+      await client.get_channel(LOG_CH_ID).send(msg)
+      
+    elif after.channel is None:
+      msg = f'{now:%m/%d-%H:%M}｜**{member.name}**が<#{before.channel.id}>から退出しました。'
+      await client.get_channel(LOG_CH_ID).send(msg)
+    
+
+@client.event
+async def on_member_join(member):
+  await client.get_channel(LOG_CH_ID).send(f'<@{member.id}>がサーバーに参加しました。自己紹介してください。')
+
+
+client.run(os.getenv('TOKEN'))
