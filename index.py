9b5be0b4b75f9719cc911076a61e596275c29893
Izu-TABI <92037081+Izu-TABI@users.noreply.github.com>
2023-02-05 09:17:03 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from datetime import datetime, timedelta
+from server import keep_alive
 
 client=discord.Client(intents=discord.Intents.all())
 
@@ -28,11 +29,14 @@ async def on_voice_state_update(member, before, after) :
   
   if member.guild.id == GUILD_ID and (before.channel != after.channel):
     if before.channel is None:
-      msg = f'{now:%m/%d-%H:%M}｜**{member.name}**が<#{after.channel.id}>に参加しました。'
+      msg = f'{now:%m/%d-%H:%M} **{member.name}**が<#{after.channel.id}>に参加しました。'
       await client.get_channel(LOG_CH_ID).send(msg)
       
     elif after.channel is None:
-      msg = f'{now:%m/%d-%H:%M}｜**{member.name}**が<#{before.channel.id}>から退出しました。'
+      msg = f'{now:%m/%d-%H:%M} **{member.name}**が<#{before.channel.id}>から退出しました。'
+      await client.get_channel(LOG_CH_ID).send(msg)
+    else:
+      msg = f'{now:%m/%d-%H:%M} **{member.name}**が<#{before.channel.id}>から<#{after.channel.id}>へ移動しました。'
       await client.get_channel(LOG_CH_ID).send(msg)
     
 
@@ -40,5 +44,5 @@ async def on_voice_state_update(member, before, after) :
 async def on_member_join(member):
   await client.get_channel(LOG_CH_ID).send(f'<@{member.id}>がサーバーに参加しました。自己紹介してください。')
 
-
+keep_alive()
 client.run(os.getenv('TOKEN'))
