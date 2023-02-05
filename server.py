27517260387e40b13c42f24acd8bdd5d28a4263c
Izu-TABI <92037081+Izu-TABI@users.noreply.github.com>
2023-02-05 21:31:02 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from threading import Thread
+import random
 
-app = Flask("")
+app = Flask("__name__")
 
 @app.route("/")
 def main():
   return "online"
 
-def run():
-  app.run("0.0.0.0", port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+  if __name__ == "__main__":  
+	  app.run( 
+		  host='0.0.0.0', 
+		  port = random.randint(2000, 9000)
+	  )
